cf9584de9a1c93e623869c09024b8f2c616c249d
longb <longb@bell.ai>
2022-09-30 18:05:08 +0800

    .

---
+++ smthit4-mybatis-plus/src/main/java/cn/smthit/v4/mybatis/plus/PageDataKit.java
@@ -0,0 +1,67 @@
+package cn.smthit.v4.mybatis.plus;
+
+import cn.smthit.v4.common.lang.convert.AbstractConvert;
+import cn.smthit.v4.common.lang.convert.DefaultConvert;
+import cn.smthit.v4.common.lang.data.PageData;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import java.util.List;
+
+/**
+ * @description: ...
+ * @author: Bean
+ * @date: 2022/9/30  15:32
+ */
+public class PageDataKit {
+
+    /**
+     * 创建一个Page对象
+     * @param <T>
+     * @return
+     */
+    public static <T> PageData<T> newPage() {
+        return new PageData<T>();
+    }
+
+    public static <T> PageData<T> newPage(int currentPage, int pageCount, long total, List<T> rows) {
+        return new PageData<T>(currentPage, pageCount, total, rows);
+    }
+
+    public static <PO, VO> PageData<VO> newPage(Page page, AbstractConvert<PO, VO> convert) {
+
+        PageData<VO> pageData = new PageData<>();
+
+        pageData.setPageCount(page.getPages());
+        pageData.setTotal(page.getTotal());
+        pageData.setCurrentPage(page.getCurrent());
+
+        pageData.setRows(convert.toVOs(page.getRecords()));
+
+        return pageData;
+    }
+
+    public static <PO, VO> PageData<VO> newPage(Page page, Class<VO> voCls) {
+        DefaultConvert<PO, VO> dc = new DefaultConvert<>(voCls);
+
+        PageData<VO> pageData = new PageData<>();
+
+        pageData.setPageCount(page.getPages());
+        pageData.setTotal(page.getTotal());
+        pageData.setCurrentPage(page.getCurrent());
+        pageData.setRows(dc.toVOs(page.getRecords()));
+
+        return pageData;
+    }
+
+    public static <VO> PageData<VO> newPage(Page page) {
+
+        PageData<VO> pageData = new PageData<>();
+
+        pageData.setPageCount(page.getPages());
+        pageData.setTotal(page.getTotal());
+        pageData.setCurrentPage(page.getCurrent());
+        pageData.setRows(page.getRecords());
+
+        return pageData;
+    }
+}

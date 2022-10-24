ef61cdddc053cd8debb14877228ece8c9f526c6d
longb <longb@bell.ai>
2022-10-24 11:11:09 +0800

    .

--- smthit4-rabbitmq/src/main/java/cn/smthit/v4/mq/rabbitmq/config/RabbitConfiguration.java
+++ smthit4-rabbitmq/src/main/java/cn/smthit/v4/mq/rabbitmq/config/RabbitConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
-public class RabbitConfigration {
+public class RabbitConfiguration {
 
     @Bean
     public RabbitTemplate createRabbitTemplate(ConnectionFactory factory) {

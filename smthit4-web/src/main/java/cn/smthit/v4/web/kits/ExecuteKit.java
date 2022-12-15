59c60b6d3946eabd31e7e40c6355abe12ce5d81c
longb <longb@bell.ai>
2022-12-15 13:21:44 +0800

    .

--- smthit4-web/src/main/java/cn/smthit/v4/web/kits/ExecuteKit.java
+++ smthit4-web/src/main/java/cn/smthit/v4/web/kits/ExecuteKit.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.support.TransactionSynchronizationAdapter;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.concurrent.*;
+
 /**
  * @description: 执行任务小工具
  * @author: Bean
@@ -11,6 +13,14 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  */
 @Slf4j
 public class ExecuteKit {
+
+    private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 20,
+            60, TimeUnit.SECONDS,
+            new LinkedBlockingDeque<>(1000),
+            new ThreadPoolExecutor.CallerRunsPolicy());
+
+    //private static ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(20);
+
     /**
      * 如果在事务中，则事务提交之后执行task
      * 如果不在事务中，则直接执行
@@ -38,7 +48,38 @@ public class ExecuteKit {
         }
     }
 
+    /**
+     * 事务提交后异步执行
+     * @param task
+     * @param async
+     */
     public static void postAfterCommit(TaskAfterCommit task, boolean async) {
-        return;
+        if(task == null) {
+            return;
+        }
+
+        if(async == true) {
+            if(TransactionSynchronizationManager.isActualTransactionActive()) {
+                TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+                    @Override
+                    public void afterCommit() {
+                        threadPoolExecutor.execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    task.execute();
+                                } catch (Exception exp) {
+                                    log.error(exp.getMessage(), exp);
+                                }
+                            }
+                        });
+                    }
+                });
+            } else {
+                task.execute();
+            }
+        } else {
+            postAfterCommit(task);
+        }
     }
 }

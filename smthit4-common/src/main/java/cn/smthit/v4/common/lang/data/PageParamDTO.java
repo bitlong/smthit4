c8941cad7e70edc3627e25d72c4bcf00284a2a8a
bitlong <longbin@mitee.cn>
2023-09-21 09:55:09 +0800

    .

--- smthit4-common/src/main/java/cn/smthit/v4/common/lang/data/PageParamDTO.java
+++ smthit4-common/src/main/java/cn/smthit/v4/common/lang/data/PageParamDTO.java
@@ -4,6 +4,8 @@
 package cn.smthit.v4.common.lang.data;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
@@ -11,15 +13,25 @@ import javax.validation.constraints.NotNull;
  * @author Bean
  *
  */
-@Data
-public class PageParamDTO extends ParamDTO {
+@Getter
+public class PageParamDTO<T> extends ParamDTO {
     /**
      * 当前页
      */
-    private int pageNo;
+    private int pageNumber;
 
     /**
      * 每页大小
      */
     private int pageSize;
+
+    public T setPageNumber(int pageNo) {
+        this.pageNumber = pageNo;
+        return (T)this;
+    }
+
+    public T setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+        return (T)this;
+    }
 }
